a4009e3609729db747cf640ceb67b3455d1bc8c9
Ben Norton <ben.norton@uk.zurich.com>
2021-03-25 17:17:50 +0000

    Changed dynamic selection buttons to use a drop-down instead. Removed the board ID from the UI as not necessary for the user to know this.

--- todo_app/app.py
+++ todo_app/app.py
@@ -11,12 +11,10 @@ boards = trello_api_client.get_boards()
 
 @app.route('/')
 def index():
-    default_board = boards[0]
-    selected_board_id = default_board.id
-    selected_board_name = default_board.name
-    selected_board_items = trello_api_client.get_items_on_a_board(selected_board_id)
-    selected_board_lists = trello_api_client.get_lists_on_a_board(selected_board_id)
-    return render_template('index.html', selected_board_id = selected_board_id, selected_board_name = selected_board_name, selected_board_lists = selected_board_lists, selected_board_items = selected_board_items, boards = boards)
+    selected_board = boards[0]
+    selected_board_items = trello_api_client.get_items_on_a_board(selected_board.id)
+    selected_board_lists = trello_api_client.get_lists_on_a_board(selected_board.id)
+    return render_template('index.html', selected_board = selected_board, selected_board_lists = selected_board_lists, selected_board_items = selected_board_items, boards = boards)
 
 @app.route('/select_board/<id>', methods=['GET'])
 def select_board(id):
@@ -25,21 +23,18 @@ def select_board(id):
             selected_board = board
             break
 
-    selected_board_id = selected_board.id
-    selected_board_name = selected_board.name
-    selected_board_items = trello_api_client.get_items_on_a_board(selected_board_id)
-    selected_board_lists = trello_api_client.get_lists_on_a_board(selected_board_id)
-    return render_template('index.html', selected_board_id = selected_board_id, selected_board_name = selected_board_name, selected_board_lists = selected_board_lists, selected_board_items = selected_board_items, boards = boards)
+    selected_board_items = trello_api_client.get_items_on_a_board(selected_board.id)
+    selected_board_lists = trello_api_client.get_lists_on_a_board(selected_board.id)
+    return render_template('index.html', selected_board = selected_board, selected_board_lists = selected_board_lists, selected_board_items = selected_board_items, boards = boards)
 
-@app.route('/create-todo', methods=['POST'])
-def create_todo():
-    board = request.form.get('board-id')
+@app.route('/create-todo/<board_id>', methods=['POST'])
+def create_todo(board_id):
     title = request.form.get('title')
     desc = request.form.get('description')
     due = request.form.get('due-date')
     list = request.form.get('list')
     trello_api_client.create_item(list, title, desc, due)
-    return redirect(f'/select_board/{board}')
+    return redirect(f'/select_board/{board_id}')
 
 @app.route('/update_status/<board_id>/<list_id>/<id>', methods=['POST'])
 def update_status(board_id, list_id, id):
