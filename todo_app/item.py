46090ddf272136a7c223321baac7de8a3c449ad7
Ben Norton <norton.red4@gmail.com>
2022-02-22 09:07:19 +0000

    Migrated data backend from Trello to MongoDB

--- todo_app/item.py
+++ todo_app/item.py
@@ -1,9 +1,17 @@
+from datetime import datetime
+
 class Item():
-    def __init__(self, id, name, description, due_date, id_list, id_board, status='To Do'):
+    def __init__(self, id, name, description, due_date, status='To Do'):
         self.id = id
         self.name = name
         self.description = description
         self.due_date = due_date
-        self.id_list = id_list
-        self.id_board = id_board
         self.status = status
+
+    @staticmethod
+    def datetime_formatted_as_date(date_time):
+        if date_time != None or date_time != '':
+            date_time = datetime.strptime(date_time, '%Y-%m-%d').strftime('%d/%m/%Y')
+        else:
+            date_time = ''
+        return date_time
